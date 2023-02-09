7c6c30e5ceffe0add789800bfc3bd4b702c0ec61
Moinuddin Ahmad <moinuddin.ahmad@waveinformatica.com>
2023-02-09 09:03:06 +0100

    initialize git

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,117 @@
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 import { Component } from '@angular/core';
+import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
-@Component({
+@Component({ 
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl:'./app.component.html',
+  styleUrls:['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  
+  public Editor = ClassicEditor;
+  public ckEditoConfig = {toolbar: [ 'heading',
+  '|',
+  'bold',
+  'italic',
+  '|',
+  'bulletedList',
+  'numberedList',
+  '|',
+  'insertTable',
+  '|',
+  'undo',
+  'redo' ]};
+
+  questions: any = [];
+  newQuestion = '';
+  newOption: any;
+  showQuestionModel = false;
+  questionError = '';
+  optionError = '';
+
+  addOption(question: {
+    showInput: boolean; options: { option: any; value: any; }[]; newOption: string; 
+}) {
+    if (!question.newOption) {
+      this.optionError = 'Option field is required';
+      return;
+    }
+
+    this.optionError = '';
+    question.options.push({ option: question.newOption, value: question.newOption });
+    question.newOption = '';
+    question.showInput = false;
+  }
+
+  addQuestion() {
+    if (!this.newQuestion || !this.newOption) {
+      this.questionError = 'Question and option fields are required';
+      return;
+    }
+
+    this.questionError = '';
+    this.questions.push({ 
+      question: this.newQuestion,
+      options: [{ option: this.newOption, value: this.newOption }],
+      showInput: false
+    });
+    this.newQuestion = '';
+    this.newOption = '';
+  }
+  
+  toggleOptionInput(question: { showInput: boolean; }) {
+    question.showInput = !question.showInput;
+  }
+
+  toggleQuestionModal() {
+    this.showQuestionModel = !this.showQuestionModel;
+  }
+  
+  removeOption(questionIndex: string | number, optionIndex: any) {
+    this.questions[questionIndex].options.splice(optionIndex, 1);
+  }
+
+  removeQuestion(questionIndex: number) {
+    this.questions.splice(questionIndex, 1);
+  }
+
+  dropQuestion(event: CdkDragDrop<{ text: string; options: any[]; newOption: string; }>) {
+    moveItemInArray(this.questions, event.previousIndex, event.currentIndex);
+  }
+  dropOption(questionIndex:number, event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.questions[questionIndex].options, event.previousIndex, event.currentIndex);
+  }
+  saveChanges() {
+    const data = {
+      questions: this.questions
+    };
+  
+    const jsonData = JSON.stringify(data);
+  console.log(jsonData);
+  
+    // save jsonData to a file or store it in a database
+  }
 }
+// ClassicEditor.defaultConfig = {
+//   toolbar: {
+//     items: [
+//       'heading',
+//       '|',
+//       'bold',
+//       'italic',
+//       '|',
+//       'bulletedList',
+//       'numberedList',
+//       '|',
+//       'insertTable',
+//       '|',
+//       'undo',
+//       'redo'
+//     ]
+//   },
+//   table: {
+//     contentToolbar: [ 'tableColumn', 'tableRow', 'mergeTableCells' ]
+//   },
+//   language: 'en'
+// };
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    DragDropModule,
+    CKEditorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
