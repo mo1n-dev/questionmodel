b2abd72cfafda1c5084f04c10accb8b7f2ce6633
Moinuddin Ahmad <moinuddin.ahmad@waveinformatica.com>
2023-02-14 09:52:41 +0100

    added table for options

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,109 +1,3 @@
-// import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
-// import { Component } from '@angular/core';
-// import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-
-// @Component({ 
-//   selector: 'app-root',
-//   templateUrl:'./app.component.html',
-//   styleUrls:['./app.component.css']
-// })
-// export class AppComponent {
-  
-//   public Editor = ClassicEditor;
-
-//   questions: any = [];
-//   newQuestion = '';
-//   newOption: any;
-//   showQuestionModel = false;
-//   questionError = '';
-//   optionError = '';
-
-//   addOption(question: {
-//     showInput: boolean; options: { option: any; value: any; }[]; newOption: string; 
-// }) {
-//     if (!question.newOption) {
-//       this.optionError = 'Option field is required';
-//       return;
-//     }
-
-//     this.optionError = '';
-//     question.options.push({ option: question.newOption, value: question.newOption });
-//     question.newOption = '';
-//     question.showInput = false;
-//   }
-
-//   addQuestion() {
-//     if (!this.newQuestion || !this.newOption) {
-//       this.questionError = 'Question and option fields are required';
-//       return;
-//     }
-
-//     this.questionError = '';
-//     this.questions.push({ 
-//       question: this.newQuestion,
-//       options: [{ option: this.newOption, value: this.newOption }],
-//       showInput: false
-//     });
-//     this.newQuestion = '';
-//     this.newOption = '';
-//   }
-  
-//   toggleOptionInput(question: { showInput: boolean; }) {
-//     question.showInput = !question.showInput;
-//   }
-
-//   toggleQuestionModal() {
-//     this.showQuestionModel = !this.showQuestionModel;
-//   }
-  
-//   removeOption(questionIndex: string | number, optionIndex: any) {
-//     this.questions[questionIndex].options.splice(optionIndex, 1);
-//   }
-
-//   removeQuestion(questionIndex: number) {
-//     this.questions.splice(questionIndex, 1);
-//   }
-
-//   dropQuestion(event: CdkDragDrop<{ text: string; options: any[]; newOption: string; }>) {
-//     moveItemInArray(this.questions, event.previousIndex, event.currentIndex);
-//   }
-//   dropOption(questionIndex:number, event: CdkDragDrop<string[]>) {
-//     moveItemInArray(this.questions[questionIndex].options, event.previousIndex, event.currentIndex);
-//   }
-//   saveChanges() {
-//     const data = {
-//       questions: this.questions
-//     };
-  
-//     const jsonData = JSON.stringify(data);
-//   console.log(jsonData);
-  
-//     // save jsonData to a file or store it in a database
-//   }
-// }
-// // ClassicEditor.defaultConfig = {
-// //   toolbar: {
-// //     items: [
-// //       'heading',
-// //       '|',
-// //       'bold',
-// //       'italic',
-// //       '|',
-// //       'bulletedList',
-// //       'numberedList',
-// //       '|',
-// //       'insertTable',
-// //       '|',
-// //       'undo',
-// //       'redo'
-// //     ]
-// //   },
-// //   table: {
-// //     contentToolbar: [ 'tableColumn', 'tableRow', 'mergeTableCells' ]
-// //   },
-// //   language: 'en'
-// // };
-
 import { Component } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
@@ -152,9 +46,7 @@ export class AppComponent {
     const question = {
       question: this.questionForm.value.question,
       options: [
-        {
-          option: this.questionForm.value.option
-        }
+        { option: this.questionForm.value.option}
       ],
       showInput: false
     };
@@ -167,7 +59,6 @@ export class AppComponent {
     question.showInput = false;
     this.optionForm.reset();
   }
-
   removeQuestion(index: number) {
     this.questions.splice(index, 1);
   }
@@ -197,4 +88,78 @@ export class AppComponent {
   }
 }
 
+// import { Component, OnInit } from '@angular/core';
+// import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+// @Component({
+//   selector: 'app-root',
+//   template: `
+//     <form [formGroup]="questionForm">
+//       <div>
+//         <label for="question">Question:</label>
+//         <input type="text" id="question" formControlName="question">
+//       </div>
+//       <div>
+//         <label for="option1">Option 1:</label>
+//         <input type="text" id="option1" formControlName="option1">
+//       </div>
+//       <div>
+//         <label for="option2">Option 2:</label>
+//         <input type="text" id="option2" formControlName="option2">
+//       </div>
+//       <button (click)="addQuestion()">Save</button>
+//     </form>
+
+//     <h3>Questions and Options:</h3>
+//     <ul>
+//       <li *ngFor="let q of questions">
+//         {{q.question}}
+//         <ul>
+//           <li *ngFor="let o of q.options">{{o}}</li>
+//         </ul>
+//         <div>
+//           <input type="text" [(ngModel)]="newOption">
+//           <button (click)="addOption(q, newOption)">Add Option</button>
+//         </div>
+//       </li>
+//     </ul>
+//     <button (click)="save()"> save changes</button>
+//   `
+// })
+// export class AppComponent implements OnInit {
+//   questionForm!: FormGroup;
+//   questions: any[] = [];
+//   newOption!: string;
+//   optionForm!: FormGroup;
+//   ngOnInit() {
+//     this.questionForm = new FormGroup({
+//       question: new FormControl('', Validators.required),
+//       option1: new FormControl('', Validators.required),
+//       option2: new FormControl('', Validators.required)
+//     });
+//     this.optionForm = new FormGroup({
+//             newOption: ['', Validators.required]
+//           });
+//   }
+//     toggleOptionInput(question: { showInput: boolean; }) {
+//     question.showInput = !question.showInput;
+//     this.optionForm.reset();
+//   }
+//   addQuestion() {
+//     this.questions.push({
+//       question: this.questionForm.value.question,
+//       options: [this.questionForm.value.option1, this.questionForm.value.option2]
+//     });
+
+//     this.questionForm.reset();
+//   }
+
+//   addOption(question: { options: any[]; }, option: any) {
+//     question.options.push(option);
+//   }
+//   save(){
+//     console.log(this.questions);
+    
+//   }
+// }
 
