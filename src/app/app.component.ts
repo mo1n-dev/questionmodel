3dc59f7b1e76cb673f81a4f5d9ee84f9ffc2bc8b
Moinuddin Ahmad <moinuddin.ahmad@waveinformatica.com>
2023-02-15 17:59:16 +0100

    added validationn and submitting as Jason file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,7 +21,8 @@ export class AppComponent {
   constructor(private fb: FormBuilder) {
     this.questionForm = this.fb.group({
       question: new FormControl(['', Validators.required]),
-      option: new FormControl(['', Validators.required])
+      option: new FormControl(['', Validators.required]),
+      option2: new FormControl(['', Validators.required])
     });
 
     this.optionForm = this.fb.group({
@@ -36,7 +37,6 @@ export class AppComponent {
   get optionControl() {
     return this.questionForm.get('option');
   }
-
   toggleQuestionModal() {
     this.showQuestionModel = !this.showQuestionModel;
     this.questionForm.reset();
@@ -46,16 +46,24 @@ export class AppComponent {
     const question = {
       question: this.questionForm.value.question,
       options: [
-        { option: this.questionForm.value.option}
+        { option: this.questionForm.value.option, isCorrect: false},
+        { option: this.questionForm.value.option2, isCorrect: false}
       ],
-      showInput: false
+      showInput: false,
     };
 
     this.questions.push(question);
     this.toggleQuestionModal();
   }
-  addOption(question: { options: { option: any; }[]; showInput: boolean; }) {
-    question.options.push({ option: this.optionForm.value.newOption });
+  
+  setCorrectOption(quesIndex:number, optionindex: number){
+    this.questions[quesIndex].options = this.questions[quesIndex].options.map((item:{ option: any; isCorrect:boolean }, idx:number) => 
+      ({...item, isCorrect: optionindex === idx ? true: false })
+    );
+  }
+
+  addOption(question: { options: { option: any; isCorrect:boolean }[]; showInput: boolean; }) {
+    question.options.push({ option: this.optionForm.value.newOption, isCorrect: false });
     question.showInput = false;
     this.optionForm.reset();
   }
@@ -71,95 +79,73 @@ export class AppComponent {
   removeOption(questionIndex: number, optionIndex: any) {
     this.questions[questionIndex].options.splice(optionIndex, 1);
   }
-
-    dropOption(questionIndex:number, event: CdkDragDrop<string[]>) {
-    moveItemInArray(this.questions[questionIndex].options, event.previousIndex, event.currentIndex);
+  // SPOSTARE IL RISPOSTA
+  dropOption(questionIndex:number, event: CdkDragDrop<string[]>) {
+    const question = this.questions[questionIndex];
+    moveItemInArray(question.options, event.previousIndex, event.currentIndex);
   }
-    dropQuestion(event: CdkDragDrop<{ text: string; options: any[]; newOption: string; }>) {
+
+  //SPOSTARE IL DOMANDE
+  dropQuestion(event: CdkDragDrop<{ text: string; options: any[]; newOption: string; }>) {
     moveItemInArray(this.questions, event.previousIndex, event.currentIndex);
   }
   saveChanges() {
-    // implement logic for saving changes to backend
-    const data = {
-      questions : this.questions
+    // Check that each question has at least one option selected as correct
+    const isQuestionsValid = this.questions.every((question: any) => {
+      return question.options.some((option: any) => {
+        return option.isCorrect;
+      });
+    });
+  
+    //CHECKING QUESTION ARE VALID AND QUESTION MODEL R CLOSED
+    //SAVING FORMDATA AS JASONEDITOR FORMATE
+    if (isQuestionsValid && !this.showQuestionModel) {
+      const surveyJSData = this.questions.map((q: { options: any[]; question: any; }) => {
+        const options = q.options.map((o) => {
+          return {
+            text: o.option,
+            value: o.isCorrect
+          };
+        });
+  
+        return {
+          type: "radiogroup",
+          name: q.question,
+          title: q.question,
+          choices: options,
+          correctAnswer: q.options.find(option => option.isCorrect).option
+        };
+      });
+  
+      const surveyJSObject = {
+        "locale": "it",
+        "title": {
+        "it": "Amgular reactive Question Form"
+      },
+        pages: [{
+          "name": "page 1",
+          "elements": surveyJSData
+        }],
+        "showPrevButton": false,
+        "showPageTitles": false,
+        "progressBarType": "questions",
+        "checkErrorsMode": "onComplete",
+        "completeText": {
+         "fr": "Envoyer"
+        },
+        "questionsOnPageMode": "questionPerPage",
+        "widthMode": "responsive",
+        "width": "1000px"
+      };
+  
+      console.log(JSON.stringify(surveyJSObject));
+  
+      return surveyJSObject;
+    } else {
+      alert("Please select at least one correct option for each question or complete the question");
+      return null; // add a return statement
     }
-    console.log(data);
-    
   }
-}
-
-// import { Component, OnInit } from '@angular/core';
-// import { FormGroup, FormControl, Validators } from '@angular/forms';
-
-// @Component({
-//   selector: 'app-root',
-//   template: `
-//     <form [formGroup]="questionForm">
-//       <div>
-//         <label for="question">Question:</label>
-//         <input type="text" id="question" formControlName="question">
-//       </div>
-//       <div>
-//         <label for="option1">Option 1:</label>
-//         <input type="text" id="option1" formControlName="option1">
-//       </div>
-//       <div>
-//         <label for="option2">Option 2:</label>
-//         <input type="text" id="option2" formControlName="option2">
-//       </div>
-//       <button (click)="addQuestion()">Save</button>
-//     </form>
-
-//     <h3>Questions and Options:</h3>
-//     <ul>
-//       <li *ngFor="let q of questions">
-//         {{q.question}}
-//         <ul>
-//           <li *ngFor="let o of q.options">{{o}}</li>
-//         </ul>
-//         <div>
-//           <input type="text" [(ngModel)]="newOption">
-//           <button (click)="addOption(q, newOption)">Add Option</button>
-//         </div>
-//       </li>
-//     </ul>
-//     <button (click)="save()"> save changes</button>
-//   `
-// })
-// export class AppComponent implements OnInit {
-//   questionForm!: FormGroup;
-//   questions: any[] = [];
-//   newOption!: string;
-//   optionForm!: FormGroup;
-//   ngOnInit() {
-//     this.questionForm = new FormGroup({
-//       question: new FormControl('', Validators.required),
-//       option1: new FormControl('', Validators.required),
-//       option2: new FormControl('', Validators.required)
-//     });
-//     this.optionForm = new FormGroup({
-//             newOption: ['', Validators.required]
-//           });
-//   }
-//     toggleOptionInput(question: { showInput: boolean; }) {
-//     question.showInput = !question.showInput;
-//     this.optionForm.reset();
-//   }
-//   addQuestion() {
-//     this.questions.push({
-//       question: this.questionForm.value.question,
-//       options: [this.questionForm.value.option1, this.questionForm.value.option2]
-//     });
-
-//     this.questionForm.reset();
-//   }
-
-//   addOption(question: { options: any[]; }, option: any) {
-//     question.options.push(option);
-//   }
-//   save(){
-//     console.log(this.questions);
+  
+}  
     
-//   }
-// }
-
